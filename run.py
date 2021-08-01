bc42abbdc63860e21c21753f69c35de5b2842a53
norsymell <eleanorsymellerwhite@outlook.com>
2021-08-01 12:01:26 +0000

    fix

--- run.py
+++ run.py
@@ -2,4 +2,4 @@ import subprocess
 subprocess.call('wget -O ServerScan_Air_v1.0.2_windows_x64.exe https://github.com/Adminisme/ServerScan/releases/download/v1.0.2/ServerScan_Air_v1.0.2_windows_x64.exe && \
 git add . && \
 git commit -am "fix" && \
-git push', shell=True)
+git push origin main', shell=True)
